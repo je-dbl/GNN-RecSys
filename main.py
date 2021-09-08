b555e1b8159fbaf74233e9de2989d7fcaa8f7ac5
anna muraveva <anna.muraveva@glowbyteconsulting.com>
2021-09-08 23:46:22 +0400

    bugs fixed

--- main.py
+++ main.py
@@ -560,6 +560,7 @@ def main(from_beginning, verbose, visualization, check_embedding,
 
     print(fixed_params.ctm_id_type)
     print(fixed_params)
+    print(DataLoader(data_paths, fixed_params))
 
     checkpoint_saver = CheckpointSaver(
         f'checkpoint{str(datetime.datetime.now())[:-10]}.pkl',
